8cdc81a789f03284889a5422b53cb116e6de2792
Daichi Ando <d_ando@gizumo-admin-07noMacBook-puro.local>
2019-01-15 17:43:13 +0900

    コメント欄のview作成

--- resources/views/question/show.blade.php
+++ resources/views/question/show.blade.php
@@ -5,11 +5,16 @@
 <div class="main-wrap">
   <div class="panel panel-success">
     <div class="panel-heading">
-      {{ $questions->category->name }}&nbsp;&nbsp;の質問
+      <img src="{{ $questions->user->avatar }}" class="avatar-img">&nbsp;&nbsp;
+      {{ $questions->user->name }}&nbsp;&nbsp;さんの質問
     </div>
     <div class="table-responsive">
       <table class="table table-striped table-bordered">
         <tbody>
+          <tr>
+            <th class="table-column">Category</th>
+            <td class="td-text">{{ $questions->category->name }}</td>
+          </tr>
           <tr>
             <th class="table-column">Title</th>
             <td class="td-text">{{ $questions->title }}</td>
@@ -22,9 +27,19 @@
       </table>
     </div>
   </div>
-  <div class="btn-bottom-wrapper left-side">
-    <a href="{{ route('question.index') }}" class="btn btn-success"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+
+  <div class="comment-box">
+    <div class="comment-title">
+      <img src="{{ Auth::user()->avatar }}" class="avatar-img"><p>コメントを投稿する</p>
+    </div>
+    <div class="comment-body">
+      {!! Form::textarea('comment', null, ['class' => 'form-control', 'placeholder' => 'Add your comment...']) !!}
+    </div>
+    <div class="comment-bottom">
+      <button type="submit" class="btn btn-success pull-right"><i class="fa fa-reply" aria-hidden="true"></i></i></button>
+    </div>
   </div>
+
 </div>
 
 @endsection
