65197d9abde8a391e5b7403e5e164d5415181f7d
Daichi Ando <d_ando@gizumo-admin-07noMacBook-puro.local>
2019-01-22 19:08:23 +0900

    Controller, Request, Routeの整理

--- routes/web.php
+++ routes/web.php
@@ -47,12 +47,10 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.' ,'namespace' => 'Admin'], fu
     Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
     Route::post('login', ['as' => 'login', 'uses' => 'Auth\LoginController@login']);
     Route::post('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
-  
-  
+
     Route::resource('report', DailyReportController::class, ['only' => ['index', 'show']]);
     Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
-    Route::resource('store', StoreController::class);
-  
+
     Route::resource('adminuser', AdminUserController::class);
     Route::get('adminuser/{adminuser}/mailedit', ['as' => 'adminuser.mailedit', 'AdminUserController@mailedit']);
     Route::post('adminuser/sendmail', ['as' => 'adminuser.sendmail', 'uses' => 'AdminUserController@sendmail']);
@@ -61,28 +59,18 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.' ,'namespace' => 'Admin'], fu
     Route::get('password/reset',['as' => 'password.request', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);
     Route::post('password/reset', ['as' => 'password.request', 'uses' => 'Auth\ResetPasswordController@reset']);
     Route::get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
-  
+
     Route::post('/register', ['as' => 'register', 'uses' => 'Auth\AdminRegisterController@adminRegister']);
     Route::get('/register/', 'Auth\AdminRegisterController@showAdminRegistrationForm');
-  
+
     Route::resource('user', UserController::class);
-  
+
     // access_right
     Route::get('/access_right', ['as' => 'access_right.index', 'uses' => 'AccessRightController@index']);
     Route::post('/access_right/sendSlack', ['as' => 'access_right.sendSlack', 'uses' => 'AccessRightController@sendSlack']);
     Route::get('/access_right/permission', ['as' => 'access_right.permission', 'uses' => 'AccessRightController@permission']);
     Route::post('/access_right/replyMail/{query}', ['as' => 'access_right.replyMail', 'uses' => 'AccessRightController@replyMail']);
-  
-    Route::get('rent/confirm', ['as' => 'rent.confirm', 'uses' => 'RentalItemController@confirm']);
-    Route::get('rent/update_confirm', ['as' => 'rent.updateConfirm', 'uses' => 'RentalItemController@updateConfirm']);
-    Route::put('rent/update_confirm', ['as' => 'rent.updateItems', 'uses' => 'RentalItemController@updateItems']);
-    Route::resource('rent', RentalItemController::class);
-  
-    Route::get('item_category/confirm', ['as' => 'item_category.confirm', 'uses' => 'ItemCategoryController@confirm']);
-    Route::get('item_category/update_confirm', ['as' => 'item_category.updateConfirm', 'uses' => 'ItemCategoryController@updateConfirm']);
-    Route::put('item_category/update_confirm', ['as' => 'item_category.updateCategory', 'uses' => 'ItemCategoryController@updateCategory']);
-    Route::resource('item_category', ItemCategoryController::class);
-  
+
     Route::get('question/create', ['as' => 'question.create', 'uses' => 'QuestionController@create']);
     Route::put('question/create', ['as' => 'question.updateAnswer', 'uses' => 'QuestionController@updateAnswer']);
     Route::resource('question', QuestionController::class);
